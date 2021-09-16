d21849b0a4ea63102032ff2d8cf2571263965c7d
Tuukka Koistinen <tvkoisti@gmail.com>
2021-09-16 17:35:44 +0300

    add debug functions to dump tables

--- apps/moments/src/moments_db_mnesia.erl
+++ apps/moments/src/moments_db_mnesia.erl
@@ -4,6 +4,7 @@
 -include_lib("kernel/include/logger.hrl").
 
 -export([insert_user/2, remove_user/1]).
+-export([dump_table/1, dump_all_tables/0]).
 
 insert_user(Uid, Name) ->
     F = fun() ->
@@ -29,3 +30,21 @@ remove_user(Uid) ->
                   end
         end,
     mnesia:transaction(F).
+
+%% Debug functions
+
+dump_table(Tab) ->
+    Wildpattern = mnesia:table_info(Tab, wild_pattern),
+    F = fun() -> mnesia:match_object(Wildpattern) end,
+    {atomic, Table} = mnesia:transaction(F),
+    Table.
+
+dump_all_tables()->
+    dump_all_tables(mnesia:system_info(tables), []).
+
+dump_all_tables([Table|Tail], Tables) when Table =/= schema ->
+    dump_all_tables(Tail, Tables ++ dump_table(Table));
+dump_all_tables([_|Tail], Tables) ->
+    dump_all_tables(Tail, Tables);
+dump_all_tables([], Tables) ->
+    Tables.
