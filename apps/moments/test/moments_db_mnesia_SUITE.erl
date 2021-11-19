8371892edf7a9dc1d9ea7193afe4cd52d58c677b
Tuukka Koistinen <tvkoisti@gmail.com>
2021-11-19 19:27:04 +0200

    add first test cases for moments_db_mnesia

---
+++ apps/moments/test/moments_db_mnesia_SUITE.erl
@@ -0,0 +1,30 @@
+-module(moments_db_mnesia_SUITE).
+-include_lib("common_test/include/ct.hrl").
+
+-export([all/0, init_per_suite/1, end_per_suite/1,
+         init_per_testcase/2, end_per_testcase/2]).
+-export([insert_user/1]).
+
+all() -> [insert_user].
+
+init_per_testcase(_, Config) ->
+    Config.
+
+end_per_testcase(_, _Config) ->
+    ok.
+
+init_per_suite(Config) ->
+    Priv = ?config(priv_dir, Config),
+    ok = application:set_env(mnesia, dir, Priv),
+    ok = application:start(mnesia),
+    ok = application:start(moments),
+    Config.
+
+end_per_suite(_Config) ->
+    application:stop(mnesia),
+    ok.
+
+insert_user(_Config) ->
+    {atomic, ok} = moments_db_mnesia:insert_user("uid1", "Name 1"),
+    {atomic, ok} = moments_db_mnesia:insert_user("uid2", "Name 2"),
+    {atomic, {error, user_exists}} = moments_db_mnesia:insert_user("uid1", "Name 3").
