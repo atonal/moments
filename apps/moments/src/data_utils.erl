2f1cd7dcc2c5a4e25ceaae451db90eab20964669
Tuukka Koistinen <tvkoisti@gmail.com>
2021-12-01 18:24:06 +0200

    add function to compare moment times

--- apps/moments/src/data_utils.erl
+++ apps/moments/src/data_utils.erl
@@ -1,8 +1,12 @@
 -module(data_utils).
--export([is_passed/2]).
+-export([is_passed/2, is_before/2]).
 
 -include("data_records.hrl").
 
 -spec is_passed(moment(), utc_time()) -> boolean().
 is_passed(Moment, Time) ->
     Moment#moment.next_moment < Time.
+
+-spec is_before(moment(), moment()) -> boolean().
+is_before(Moment1, Moment2) ->
+    Moment1#moment.next_moment < Moment2#moment.next_moment.
--- apps/moments/test/data_utils_tests.erl
+++ apps/moments/test/data_utils_tests.erl
@@ -11,3 +11,16 @@ is_passed_equal_test() ->
 is_passed_greater_test() ->
     Moment = #moment{ next_moment=2 },
     ?assertNot(data_utils:is_passed(Moment, 1)).
+
+is_before_test() ->
+    Moment1 = #moment{ next_moment=1 },
+    Moment2 = #moment{ next_moment=2 },
+    ?assert(data_utils:is_before(Moment1, Moment2)).
+is_before_equal_test() ->
+    Moment1 = #moment{ next_moment=1 },
+    Moment2 = #moment{ next_moment=1 },
+    ?assertNot(data_utils:is_before(Moment1, Moment2)).
+is_before_greater_test() ->
+    Moment1 = #moment{ next_moment=2 },
+    Moment2 = #moment{ next_moment=1 },
+    ?assertNot(data_utils:is_before(Moment1, Moment2)).
