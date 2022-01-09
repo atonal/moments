e3b62930581fbd3a786574baa159766320e5ed1a
Tuukka Koistinen <tvkoisti@gmail.com>
2022-01-09 15:21:22 +0200

    debug: add function to generate a dummy moment

--- apps/moments/src/moments_debug.erl
+++ apps/moments/src/moments_debug.erl
@@ -1,6 +1,10 @@
 -module(moments_debug).
 
+-include("data_records.hrl").
+
 -export([dump_table/1, dump_all_tables/0]).
+-export([gen_moment/1]).
+
 
 dump_table(Tab) ->
     Wildpattern = mnesia:table_info(Tab, wild_pattern),
@@ -17,3 +21,13 @@ dump_all_tables([_|Tail], Tables) ->
     dump_all_tables(Tail, Tables);
 dump_all_tables([], Tables) ->
     Tables.
+
+gen_moment(Next) ->
+    #moment{
+       moment_id="id1",
+       name="name 1",
+       next_moment=Next,
+       interval=1,
+       excl_days=[],
+       excl_time=[],
+       private=false}.
