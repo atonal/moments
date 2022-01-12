a013be0f515eb108b7a9e17cee12ee16aa7228f7
Tuukka Koistinen <tvkoisti@gmail.com>
2022-01-12 19:51:55 +0200

    add clear_all_tables to debug functions
    duplicate from test_utils

--- apps/moments/src/moments_debug.erl
+++ apps/moments/src/moments_debug.erl
@@ -3,8 +3,8 @@
 -include("data_records.hrl").
 
 -export([dump_table/1, dump_all_tables/0]).
+-export([clear_all_tables/0]).
 -export([gen_moment/1]).
-
 
 dump_table(Tab) ->
     Wildpattern = mnesia:table_info(Tab, wild_pattern),
@@ -31,3 +31,13 @@ gen_moment(Next) ->
        excl_days=[],
        excl_time=[],
        private=false}.
+
+clear_all_tables()->
+    clear_all_tables(mnesia:system_info(tables)).
+clear_all_tables([Table|Tail]) when Table =/= schema ->
+    {atomic, ok} = mnesia:clear_table(Table),
+    clear_all_tables(Tail);
+clear_all_tables([_|Tail]) ->
+    clear_all_tables(Tail);
+clear_all_tables([]) ->
+    ok.
