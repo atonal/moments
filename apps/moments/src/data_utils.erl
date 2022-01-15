0c051c3f06a83f5c4d269a3e8e378b092316294f
Tuukka Koistinen <tvkoisti@gmail.com>
2022-01-15 15:59:52 +0200

    add simple helper funs to add time units together

--- apps/moments/src/data_utils.erl
+++ apps/moments/src/data_utils.erl
@@ -1,5 +1,5 @@
 -module(data_utils).
--export([is_passed/2, is_before/2]).
+-export([is_passed/2, is_before/2, time_add/2, get_next_moment/1]).
 
 -include("data_records.hrl").
 
@@ -10,3 +10,55 @@ is_passed(Moment, Time) ->
 -spec is_before(moment(), moment()) -> boolean().
 is_before(Moment1, Moment2) ->
     Moment1#moment.next_moment < Moment2#moment.next_moment.
+
+-define(sec_to_min(S), (60 * S)).
+-define(sec_to_hour(S), (60 * ?sec_to_min(S))).
+-define(sec_to_day(S), (24 * ?sec_to_hour(S))).
+-define(sec_to_week(S), (7 * ?sec_to_day(S))).
+-define(sec_to_month(S), (30 * ?sec_to_day(S))).
+-define(sec_to_year(S), (365 * ?sec_to_day(S))).
+
+-type second() :: {second, non_neg_integer()}.
+-type minute() :: {minute, non_neg_integer()}.
+-type hour() :: {hour, non_neg_integer()}.
+-type day() :: {day, non_neg_integer()}.
+-type week() :: {week, non_neg_integer()}.
+-type month() :: {month, non_neg_integer()}.
+-type year() :: {year, non_neg_integer()}.
+-type timeinterval() :: second() | minute() | hour() | day() | week() | month() | year().
+
+-spec time_add(utc_time(), timeinterval()) -> utc_time().
+time_add(Time, {Unit, Value}) ->
+    case Unit of
+        second ->
+            Time + Value;
+        minute ->
+            Time + ?sec_to_min(Value);
+        hour ->
+            Time + ?sec_to_hour(Value);
+        day ->
+            Time + ?sec_to_day(Value);
+        week ->
+            Time + ?sec_to_week(Value);
+        month ->
+            Time + ?sec_to_month(Value);
+        year ->
+            Time + ?sec_to_year(Value)
+    end.
+
+-spec get_next_moment(moment()) -> next_moment().
+get_next_moment(#moment{next_moment=Next, interval=Interval}) ->
+    case Interval of
+        debug ->
+            time_add(Next, {minute, 1});
+        hourly ->
+            time_add(Next, {hour, 1});
+        daily ->
+            time_add(Next, {day, 1});
+        weekly ->
+            time_add(Next, {week, 1});
+        monthly ->
+            time_add(Next, {month, 1});
+        yearly ->
+            time_add(Next, {year, 1})
+    end.
--- apps/moments/test/data_utils_tests.erl
+++ apps/moments/test/data_utils_tests.erl
@@ -15,3 +15,28 @@ is_before() ->
     [{"before", ?_assert(data_utils:is_before(#moment{ next_moment=1 }, #moment{ next_moment=2 }))},
      {"equals", ?_assertNot(data_utils:is_before(#moment{ next_moment=1 }, #moment{ next_moment=1 }))},
      {"after", ?_assertNot(data_utils:is_before(#moment{ next_moment=2 }, #moment{ next_moment=1 }))}].
+
+time_add_test_() ->
+    {"time_add tests", time_add()}.
+time_add() ->
+    [{"second", ?_assertEqual(2, data_utils:time_add(1, {second, 1}))},
+     {"seconds", ?_assertEqual(3, data_utils:time_add(1, {second, 2}))},
+     {"minute", ?_assertEqual(61, data_utils:time_add(1, {minute, 1}))},
+     {"minutes", ?_assertEqual(181, data_utils:time_add(1, {minute, 3}))},
+     {"hour", ?_assertEqual(3601, data_utils:time_add(1, {hour, 1}))},
+     {"day", ?_assertEqual(86_401, data_utils:time_add(1, {day, 1}))},
+     {"week", ?_assertEqual(604_801, data_utils:time_add(1, {week, 1}))},
+     {"month", ?_assertEqual(2_592_001, data_utils:time_add(1, {month, 1}))},
+     {"year", ?_assertEqual(31_536_001, data_utils:time_add(1, {year, 1}))}].
+
+get_next_moment_test_() ->
+    {"get_next_moment tests", get_next_moment()}.
+get_next_moment() ->
+    [
+     {"debug", ?_assertEqual(61, data_utils:get_next_moment(#moment{ next_moment=1, interval=debug }))},
+     {"hourly", ?_assertEqual(3601, data_utils:get_next_moment(#moment{ next_moment=1, interval=hourly }))},
+     {"daily", ?_assertEqual(86_401, data_utils:get_next_moment(#moment{ next_moment=1, interval=daily }))},
+     {"weekly", ?_assertEqual(604_801, data_utils:get_next_moment(#moment{ next_moment=1, interval=weekly }))},
+     {"monthly", ?_assertEqual(2_592_001, data_utils:get_next_moment(#moment{ next_moment=1, interval=monthly }))},
+     {"yearly", ?_assertEqual(31_536_001, data_utils:get_next_moment(#moment{ next_moment=1, interval=yearly }))}
+    ].
