dd6502dd8b015c96525686987e2cd0e33f94fb31
Tuukka Koistinen <tvkoisti@gmail.com>
2022-01-22 14:25:28 +0200

    webserver: hello world cowboy server

---
+++ apps/webserver/src/base_h.erl
@@ -0,0 +1,10 @@
+-module(base_h).
+
+-export([init/2]).
+
+init(Req0, State) ->
+    Req = cowboy_req:reply(200,
+                           #{<<"content-type">> => <<"text/plain">>},
+                           <<"Hello world!">>,
+                           Req0),
+    {ok, Req, State}.
--- apps/webserver/src/webserver_app.erl
+++ apps/webserver/src/webserver_app.erl
@@ -10,6 +10,10 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+                                      {'_', [{"/", base_h, []}]}
+                                     ]),
+    {ok, _} = cowboy:start_clear(base_listener, [{port, 8080}], #{env => #{dispatch => Dispatch}}),
     webserver_sup:start_link().
 
 stop(_State) ->
