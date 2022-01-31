28ac333206b1a22ea7bbc78ce318079620729e35
Tuukka Koistinen <tvkoisti@gmail.com>
2022-01-31 18:54:39 +0200

    add function specs to event_sub

--- apps/moments/src/event_sub.erl
+++ apps/moments/src/event_sub.erl
@@ -5,19 +5,26 @@
 
 -export([init/1, handle_event/2, handle_call/2, terminate/2]).
 
+-type state() :: {pid(), moment()}.
+
+-spec init(state()) -> {ok, state()}.
 init(State) ->
     {ok, State}.
 
-handle_event(Event = {moment, Moment=#moment{moment_id=MomentId}}, State = {Pid, MomentId}) ->
+-spec handle_event(term(), state()) -> {ok, state()}.
+handle_event(Event = {moment, Moment=#moment{moment_id=MomentId}},
+             State = {Pid, MomentId}) ->
     ?LOG_NOTICE("event_sub: moment: ~p", [Moment]),
     Pid ! Event,
     {ok, State};
 handle_event(_Event, State) ->
     {ok, State}.
 
+-spec handle_call(term(), state()) -> {ok, ok, state()}.
 handle_call(Request, State) ->
     ?LOG_NOTICE("event_sub: call ~p", [Request]),
     {ok, ok, State}.
 
+-spec terminate(term(), state()) -> ok.
 terminate(_Args, _State) ->
     ok.
