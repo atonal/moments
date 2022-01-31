6a9cbfd07d8c17502eac532512aa97722e0ef2b8
Tuukka Koistinen <tvkoisti@gmail.com>
2022-01-31 18:48:51 +0200

    minor refactoring

--- apps/webserver/src/webserver.erl
+++ apps/webserver/src/webserver.erl
@@ -3,10 +3,11 @@
 -export([start_link/0, stop/0]).
 
 start_link() ->
-    Dispatch = cowboy_router:compile([{'_', [{"/", base_h, []},
-                                             {"/api/v1/moments", moments_h, []},
-                                             {"/api/v1/moment/:id", [{id, int}], moment_h, []}
-                                            ]}]),
+    Dispatch = cowboy_router:compile([{'_',
+                                       [{"/", base_h, []},
+                                        {"/api/v1/moments", moments_h, []},
+                                        {"/api/v1/moment/:id", [{id, int}], moment_h, []}
+                                       ]}]),
     cowboy:start_clear(http, [{port, 8080}], #{env => #{dispatch => Dispatch}}).
 
 stop() ->
