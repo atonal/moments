dfbc4138b6e2fd56c96a6fd3922a8177f2a1e519
Tuukka Koistinen <tvkoisti@gmail.com>
2022-01-31 19:57:25 +0200

    move id specific moment URI under moments/

--- apps/webserver/src/webserver.erl
+++ apps/webserver/src/webserver.erl
@@ -6,7 +6,7 @@ start_link() ->
     Dispatch = cowboy_router:compile([{'_',
                                        [{"/", base_h, []},
                                         {"/api/v1/moments", moments_h, []},
-                                        {"/api/v1/moment/:id", [{id, int}], moment_h, []}
+                                        {"/api/v1/moments/:id", [{id, int}], moment_h, []}
                                        ]}]),
     cowboy:start_clear(http, [{port, 8080}], #{env => #{dispatch => Dispatch}}).
 
