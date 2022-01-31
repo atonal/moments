a6fd000cb91c90abce8b7ef7ef4517d80ea1b91b
Tuukka Koistinen <tvkoisti@gmail.com>
2022-01-31 23:05:40 +0200

    implement dummy POST for moments/
    Request body is disregarded for now.

--- apps/webserver/src/moments_h.erl
+++ apps/webserver/src/moments_h.erl
@@ -7,7 +7,9 @@
 -export([known_methods/2]).
 -export([allowed_methods/2]).
 -export([content_types_provided/2]).
+-export([content_types_accepted/2]).
 -export([to_json/2]).
+-export([from_json/2]).
 
 init(Req, State) ->
     {cowboy_rest, Req, State}.
@@ -16,16 +18,38 @@ service_available(Req, State) ->
     {mnesia:system_info(is_running) =:= yes, Req, State}.
 
 known_methods(Req, State) ->
-	{[<<"GET">>, <<"HEAD">>, <<"OPTIONS">>], Req, State}.
+	{[<<"GET">>, <<"HEAD">>, <<"OPTIONS">>, <<"POST">>], Req, State}.
 
 allowed_methods(Req, State) ->
-	{[<<"GET">>, <<"HEAD">>, <<"OPTIONS">>], Req, State}.
+	{[<<"GET">>, <<"HEAD">>, <<"OPTIONS">>, <<"POST">>], Req, State}.
 
 content_types_provided(Req, State) ->
     {[{{ <<"application">>, <<"json">>, '*'}, to_json}], Req, State}.
+
+content_types_accepted(Req, State) ->
+    {[{{ <<"application">>, <<"json">>, '*'}, from_json}], Req, State}.
 
 to_json(Req, State) ->
     Moments = moments_db_mnesia:get_moments(),
     MomentMaps = lists:map(fun moments_data:moment_to_map/1, Moments),
     Body = jsx:encode(MomentMaps),
 	{Body, Req, State}.
+
+read_body(Req0, Acc) ->
+    case cowboy_req:read_body(Req0) of
+        {ok, Data, Req} -> {ok, << Acc/binary, Data/binary >>, Req};
+        {more, Data, Req} -> read_body(Req, << Acc/binary, Data/binary >>)
+    end.
+
+from_json(Req0, State) ->
+    {ok, ReqBody, Req} = read_body(Req0, <<>>),
+    ?LOG_NOTICE("moments_h: ReqBody: ~p", [ReqBody]),
+    % ReqMoment = jsx:decode(ReqBody),
+    Result = case moments_db_mnesia:insert_moment(<<"moment X">>, 1) of
+                 Mid when is_integer(Mid) ->
+                     {see_other, <<"api/v1/moments/", (integer_to_binary(Mid))/binary>>};
+                 {error, Err} ->
+                     cowboy_req:reply(400, #{ <<"content-type">> => <<"text/plain">>
+                                            }, [<<"Error: ">>, atom_to_list(Err)], Req)
+             end,
+	{Result, Req, State}.
