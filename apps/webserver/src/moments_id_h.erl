3135858a98a2201557d5723903d203ed7ddf1871
Tuukka Koistinen <tvkoisti@gmail.com>
2022-03-04 17:35:38 +0200

    webserver: implement resource_exists/2 for moments_id _h

--- apps/webserver/src/moments_id_h.erl
+++ apps/webserver/src/moments_id_h.erl
@@ -8,6 +8,7 @@
 -export([allowed_methods/2]).
 -export([content_types_provided/2]).
 -export([to_json/2]).
+-export([resource_exists/2]).
 
 init(Req, State) ->
     {cowboy_rest, Req, State}.
@@ -24,20 +25,19 @@ allowed_methods(Req, State) ->
 content_types_provided(Req, State) ->
     {[{{ <<"application">>, <<"json">>, '*'}, to_json}], Req, State}.
 
-to_json(Req, State) ->
+resource_exists(Req, State) ->
     MomentId = cowboy_req:binding(id, Req),
+    ?LOG_INFO("Request for moment id: ~p", [MomentId]),
     case moments_db_mnesia:get_moment_with_links(MomentId) of
         M when map_size(M) =:= 0 ->
-            ?LOG_INFO("moments_id_h got empty map"),
-            Resp = cowboy_req:reply(404,
-                                    #{<<"content-type">> => <<"application/json">>},
-                                    jsx:encode(#{error=>
-                                                 #{reason=> <<"Moment not found">>}}),
-                                    Req),
-            {[], Resp, State};
+            ?LOG_DEBUG("Moment not found: ~p", [MomentId]),
+            {false, Req, State};
         Moment ->
-            ?LOG_INFO("moments_id_h got map: ~p", [Moment]),
-            MomentMap = moments_data:moments_with_links_to_jsonapi_map(Moment),
-            Body = jsx:encode(MomentMap),
-            {Body, Req, State}
+            ?LOG_DEBUG("moments_id_h got map: ~p", [Moment]),
+            {true, Req, State#{data => Moment}}
     end.
+
+to_json(Req, #{data := Moment} = State) ->
+    MomentMap = moments_data:moments_with_links_to_jsonapi_map(Moment),
+    Body = jsx:encode(MomentMap),
+    {Body, Req, State}.
